af995323eeb482c2287f50e25d2e26daf59eac33
grahametc <159605112+grahametc@users.noreply.github.com>
2025-02-18 21:55:12 -0500

    Add files via upload

--- image.py
+++ image.py
@@ -1,11 +1,12 @@
 from PIL import Image, ExifTags
-import sys, traceback
+import sys, traceback, argparse
 
 def convert_img(image, size):
     try:
         img = Image.open(image)
         img = img.resize((size, size)).convert('L')
     except:
+        print(" Usage: python3 image.py -f imagefile -s 0-200")
         traceback.print_exc()
         sys.exit() 
     if not img.getexif(): # 
@@ -19,7 +20,7 @@ def convert_img(image, size):
     for y in range(h):
         for x in range(w-1, -1, -1): # image is inverted by pillow for a reason unfamiliar to me
             color = pixels[y, x]
-            if(color > 0 and color <= 51): string+=" "
+            if(color >= 0 and color <= 51): string+=" "
             elif(color >= 52 and color <= 127): string+="*"
             elif(color >= 128 and color <= 192): string += "x"
             else: string += "#"
@@ -30,10 +31,12 @@ def convert_img(image, size):
     print(string)
     
 if __name__ == "__main__":
-    img_arg = sys.argv[1]
-    size_arg = int(sys.argv[2])
-    if(size_arg > 200): 
-        size_arg = 200
+    parser = argparse.ArgumentParser(description="Image to ascii art")
+    parser.add_argument("-file", "-f", required=True)
+    parser.add_argument("-size", "-s", default=100)
+    args = parser.parse_args()
+    if(int(args.size) > 200): 
+        args.size = 200
         print("(Max size is 200x200)")
-    convert_img(img_arg, size_arg)
+    convert_img(args.file, int(args.size))
     
